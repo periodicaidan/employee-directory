efe8095a7f209d336152d217409353fad1edcca9
Aidan T. Manning <periodicaidan@gmail.com>
2020-11-19 19:56:41 -0500

    Made it so empty cells are sorted higher than non-empty cells

--- src/components/EmployeeTable.jsx
+++ src/components/EmployeeTable.jsx
@@ -9,8 +9,13 @@ function compareEmployeesBy(field, e1, e2) {
     const p1 = e1[field];
     const p2 = e2[field];
 
-    // The fields of an employee are either a string or a number
-    if (typeof p1 === 'string') {
+    // The fields of an employee are either a string or a number or nullish
+    // Nullish values should bubble to the top of the table
+    if (p1 == null) {
+        return -1;
+    } else if (p2 == null) {
+        return 1;
+    } else if (typeof p1 === 'string') {
         // Subtracting between strings is not a thing you can do in JS
         // Why ECMA don't just define it as something like this is beyond me
         return (p1 > p2) ? 1 : -1;
